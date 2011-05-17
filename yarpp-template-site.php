057bf683c288bb482eb397f6ccc378ff30df8ecb
Matt Rude <matt@mattrude.com>
2011-05-17 08:00:17 -0500

    First Commit

---
+++ yarpp-template-site.php
@@ -0,0 +1,13 @@
+<?php /*
+Site template
+*/
+?><h3>Related Posts</h3>
+<?php if ($related_query->have_posts()):?>
+<ul>
+	<?php while ($related_query->have_posts()) : $related_query->the_post(); ?>
+	<li><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></li>
+	<?php endwhile; ?>
+</ul>
+<?php else: ?>
+<p>No related posts.</p>
+<?php endif; ?>
